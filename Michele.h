5bb38de680694adb70d1e59fd5c44473b99f1a98
Michele Acciuffi <micheleacciuffi@gmail.com>
2024-12-05 17:42:53 +0100

    Add files via upload

---
+++ Michele.h
@@ -0,0 +1,26 @@
+#ifndef MICHELE_H
+#define MICHELE_H
+
+#define RESET "\033[0m"
+
+//COLORI TESTO
+#define ROSSO "\033[31m"
+#define VERDE "\033[32m"
+#define GIALLO "\033[33m"
+#define BLU "\033[34m"
+#define CIANO "\033[36m"
+
+//SFONDI
+#define S_ROSSO "\033[41m"
+#define S_VERDE "\033[42m"
+#define S_GIALLO "\033[43m"
+#define S_BLU "\033[44m"
+
+//TESTO
+#define GRASSETTO "\033[1m"
+#define SOTTOLINEATO "\033[4m"
+#define LAMPEGGIANTE "\033[5m"
+#define INVERSIONE "\033[7m"
+
+
+#endif
