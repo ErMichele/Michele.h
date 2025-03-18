dbe10f2e40e6e24ec7833e1b7b33a7a82f6dc2ec
Michele Acciuffi <micheleacciuffi@gmail.com>
2025-03-18 10:57:55 +0100

    Added tests to verify that the library works

---
+++ Tests/Console_Format.c
@@ -0,0 +1,11 @@
+/* Test to validate the Console Text Formatting Macros functionality.
+This code demonstrates the use of macros to customize console text appearance.
+It prints "Hello, test!" to the terminal with the text color set to blue, 
+a green background, and bold formatting applied for enhanced readability. */
+
+#include <stdio.h>
+#include "../Michele.h"
+
+int main () {
+    printf(BLU S_VERDE GRASSETTO "Hello, test!" RESET);
+}
---
+++ Tests/Physical_Quantities.c
@@ -0,0 +1,26 @@
+/* Test to verify the functionalities of mathematical operations on physical quantities.
+The code creates two physical quantities with predefined values and uncertainties. 
+It then performs various operations (sum, subtraction, multiplication, division, and power) 
+and prints the resulting values and uncertainties in a readable format to the terminal. */
+
+
+#include <stdio.h>
+#include "../Michele.h"
+
+int main () {
+    struct Grandezza_Fisica Grandezza1 = {
+        10.3,
+        0.3
+    };
+    struct Grandezza_Fisica Grandezza2 = {
+        3.45,
+        0.7
+    };
+    struct Grandezza_Fisica Grandezza_Somma, Grandezza_Differenza, Grandezza_Prodotto, Grandezza_Quoziente, Grandezza_Potenza;
+    Grandezza_Somma = Somma_Fisica(Grandezza1, Grandezza2);
+    Grandezza_Differenza = Differenza_Fisica(Grandezza1, Grandezza2);
+    Grandezza_Prodotto = Prodotto_Fisica(Grandezza1, Grandezza2);
+    Grandezza_Quoziente = Divisione_Fisica(Grandezza1, Grandezza2);
+    Grandezza_Potenza = Potenza_Fisica(Grandezza1, 2);
+    printf("With the two physical quantities, after all the operations, we get:\nFor the sum: %0.2f as the value and %0.2f as the uncertainty;\nFor the subtraction: %0.2f as the value and %0.2f as the uncertainty;\nFor the multiplication: %0.2f as the value and %0.2f as the uncertainty;\nFor the division: %0.2f as the value and %0.2f as the uncertainty;\nFor the power: %0.2f as the value and %0.2f as the uncertainty. ", Grandezza_Somma.Valore, Grandezza_Somma.Incertezza, Grandezza_Differenza.Valore, Grandezza_Differenza.Incertezza, Grandezza_Prodotto.Valore, Grandezza_Prodotto.Incertezza, Grandezza_Quoziente.Valore, Grandezza_Quoziente.Incertezza, Grandezza_Potenza.Valore, Grandezza_Potenza.Incertezza);
+}
